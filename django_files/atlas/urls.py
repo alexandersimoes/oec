19e02b3edc5d7907f1a301ca75d4f9015d0d0522
Atlas Server <macro@atlas.media.mit.edu>
2012-03-09 20:10:56 -0500

    Changes for deploying on live server

--- django_files/atlas/urls.py
+++ django_files/atlas/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, include, url
+#from django.conf.urls import patterns, include
+from django.conf.urls.defaults import *
 from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('',
--- django_files/atlas/wsgi.py
+++ django_files/atlas/wsgi.py
@@ -1,28 +1,13 @@
-"""
-WSGI config for atlas project.
-
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "atlas.settings")
-
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+import os, sys
+ 
+apache_configuration= os.path.dirname(__file__)
+project = os.path.dirname(apache_configuration)
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
+ 
+sys.path.append('/usr/local/lib/python2.6/dist-packages/django/')
+sys.path.append('/home/macro/public_html/atlas_economic_complexity/django_files')
+ 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'atlas.settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
