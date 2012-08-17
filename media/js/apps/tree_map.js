30f7d764983c1485de6fb114621f60986bf424e4
Alex Simoes <alexandersimoes@gmail.com>
2012-08-17 00:29:16 -0400

    Added bottom padding for mouseover to compensate for offset used for top info box

--- media/js/apps/tree_map.js
+++ media/js/apps/tree_map.js
@@ -1,5 +1,6 @@
 function TreeMap(args){
   var _this = this;
+  this.padding = {"top": 20}
   this.attr_data = args.attr_data;
   this.raw_data = args.raw_data;
   this.selector = args.selector || window;
@@ -25,12 +26,12 @@ function TreeMap(args){
 
   this.svg = d3.select(this.selector).html("").append("svg")
     .attr("width", this.width)
-    .attr("height", this.height+20)
+    .attr("height", this.height+this.padding.top)
   this.viz = this.svg.append("g")
     .attr("class", "viz")
     .data([this.heirarchical_data])
   if(this.total == "true"){
-    this.viz.attr("transform", "translate(0, 20)");
+    this.viz.attr("transform", "translate(0, "+this.padding.top+")");
   }
 }
 
@@ -215,30 +216,30 @@ TreeMap.prototype.format_text = function(element, d){
 }
 
 TreeMap.prototype.add_mouse_events = function(){
-	var _this = this;
-	var cell = this.viz.selectAll("g")
-	cell.on("mouseover", function(d){
-		var sub_text = "Value: $" + format_big_num(d.value)[0] + format_big_num(d.value)[1]
-			sub_text += " Share: " + d3.format(".2p")(d.value / d.parent.parent.parent.value)
-			sub_text += d.data.rca ? " RCA: " + d3.format(".2r")(d.data.rca) : "";
-		var a = _this.attr_data[d.data.item_id]; // This items attributes
-		var mouseover_d = {
-			"title": a.name,
-			"img_src": a.code ? "/media/img/icons/community_"+a.category_id+".png" : "/media/img/icons/flag_"+a.name_3char.toLowerCase()+".png",
-			"sub_text": sub_text
-		}
-		make_mouseover(this, [_this.width, _this.height+20], mouseover_d)
-		// to highlight the node create new box around it
-		d3.select(this).select("rect")
-			.attr("stroke", "black")
-			.attr("stroke-width", 2)
-	})
+  var _this = this;
+  var cell = this.viz.selectAll("g")
+  cell.on("mouseover", function(d){
+    var sub_text = "Value: $" + format_big_num(d.value)[0] + format_big_num(d.value)[1]
+      sub_text += " Share: " + d3.format(".2p")(d.value / d.parent.parent.parent.value)
+      sub_text += d.data.rca ? " RCA: " + d3.format(".2r")(d.data.rca) : "";
+    var a = _this.attr_data[d.data.item_id]; // This items attributes
+    var mouseover_d = {
+      "title": a.name,
+      "img_src": a.code ? "/media/img/icons/community_"+a.category_id+".png" : "/media/img/icons/flag_"+a.name_3char.toLowerCase()+".png",
+      "sub_text": sub_text
+    }
+    make_mouseover(this, [_this.width, _this.height+_this.padding.top], mouseover_d, _this.padding.top)
+    // to highlight the node create new box around it
+    d3.select(this).select("rect")
+      .attr("stroke", "black")
+      .attr("stroke-width", 2)
+  })
   cell.on("mouseout", function(d){
-		d3.select("g.info").remove()
-		d3.select(this).select("rect")
-			.attr("stroke", "white")
-			.attr("stroke-width", 1)
-	})
+    d3.select("g.info").remove()
+    d3.select(this).select("rect")
+      .attr("stroke", "white")
+      .attr("stroke-width", 1)
+  })
 }
 
 TreeMap.prototype.add_total = function(){
@@ -322,57 +323,59 @@ function format_big_num(d){
 	}
 	return [sign+n, s];
 }
-function make_mouseover(element, dimensions, data){
-	// unpack function parameters
-	var w = dimensions[0],
-		h = dimensions[1],
-		title = data.title,
-		img_src = data.img_src,
-		sub_text = data.sub_text,
-		svg = find_parent(element, "svg");
-		
-	var info_h = 36;
-	var padding = info_h * .10;
-	// create grouping
-	var info = d3.select(svg).append("g")
-		.attr("class", "info")
-		.attr("transform", function(d){
-			var mouse_y = d3.svg.mouse(svg)[1];
-			var y = mouse_y > h - info_h ? 0 : h - info_h;
-			return "translate(0, "+y+")";
-		})
-	// create semi-transparent background
-	info.append("rect")
-		.attr("fill", "black")
-		.attr("opacity", 0.5)
-		.attr("x", 0)
-		.attr("y", 0)
-		.attr("width", w)
-		.attr("height", info_h)
-	// <image xlink:href="firefox.jpg" x="0" y="0" height="50px" width="50px"/>
-	info.append("image")
-		.attr("xlink:href", img_src)
-		.attr("x", padding)
-		.attr("y", padding)
-		.attr("height", info_h-padding*2)
-		.attr("width", info_h-padding*2)
-	info.append("text")
-		.attr("x", padding + info_h)
-		.attr("y", padding)
-		.attr("dy", info_h/2 - padding)
-		.attr("font-size", 16)
-		.attr("font-weight", "bold")
-		.attr("fill", "white")
-		.style("text-shadow", "1px 1px 2px black")
-		.text(title)
-	info.append("text")
-		.attr("x", padding + info_h)
-		.attr("y", info_h - (padding*2))
-		.attr("dy", info_h/4 - (padding*2))
-		.attr("font-size", 12)
-		.attr("fill", "white")
-		.style("text-shadow", "1px 1px 2px black")
-		.text(sub_text)
+function make_mouseover(element, dimensions, data, padding){
+  // unpack function parameters
+  var w = dimensions[0],
+    h = dimensions[1],
+    title = data.title,
+    img_src = data.img_src,
+    sub_text = data.sub_text,
+    svg = find_parent(element, "svg"),
+    info_h = 36,
+    edge_padding = info_h * .10,
+    padding = padding ? padding : 0;
+  
+  // var padding = info_h * .10;
+  // create grouping
+  var info = d3.select(svg).append("g")
+    .attr("class", "info")
+    .attr("transform", function(d){
+      var mouse_y = d3.svg.mouse(svg)[1];
+      var y = mouse_y > (h - info_h - padding) ? 0 : h - info_h - padding;
+      return "translate(0, "+y+")";
+    })
+  // create semi-transparent background
+  info.append("rect")
+    .attr("fill", "black")
+    .attr("opacity", 0.5)
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", info_h)
+  // <image xlink:href="firefox.jpg" x="0" y="0" height="50px" width="50px"/>
+  info.append("image")
+    .attr("xlink:href", img_src)
+    .attr("x", edge_padding)
+    .attr("y", edge_padding)
+    .attr("height", info_h-edge_padding*2)
+    .attr("width", info_h-edge_padding*2)
+  info.append("text")
+    .attr("x", edge_padding + info_h)
+    .attr("y", edge_padding)
+    .attr("dy", info_h/2 - edge_padding)
+    .attr("font-size", 16)
+    .attr("font-weight", "bold")
+    .attr("fill", "white")
+    .style("text-shadow", "1px 1px 2px black")
+    .text(title)
+  info.append("text")
+    .attr("x", edge_padding + info_h)
+    .attr("y", info_h - (edge_padding*2))
+    .attr("dy", info_h/4 - (edge_padding*2))
+    .attr("font-size", 12)
+    .attr("fill", "white")
+    .style("text-shadow", "1px 1px 2px black")
+    .text(sub_text)
 }
 function tmap_get_text_height(attr){
 	return function(d){
