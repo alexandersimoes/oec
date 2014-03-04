80bf03f2b58f55643840c5c17fa5ac55a47539bd
Alex Simoes <alexandersimoes@gmail.com>
2014-03-04 14:31:45 -0500

    don't show first time welcome message on embed pages

--- oec/general/views.py
+++ oec/general/views.py
@@ -44,7 +44,7 @@ def before_request():
     else:
         session['first_time'] = True
 
-    if session['first_time']:
+    if session['first_time'] and request.endpoint != "explore.embed":
         flash("Welcome! You may have noticed our recent redesign, please <a href='https://docs.google.com/forms/d/1NHkCGQR2u796RMZgo1bYUBbNPnpm1cWAX0kp6nnfwqE/viewform' target='_blank'>let us know what you think</a>.", "first_time")
 
     lang = request.args.get('lang', None)
