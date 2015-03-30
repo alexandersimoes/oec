c30d84d9a4918d01f73bff32bdeb0de96046ab6d
Alex Simoes <alexandersimoes@gmail.com>
2015-03-29 22:09:01 -0400

    added update message to homepage for all visitors

--- oec/general/views.py
+++ oec/general/views.py
@@ -44,7 +44,7 @@ def before_request():
     else:
         session['first_time'] = True
 
-    if session['first_time'] and request.endpoint != "explore.embed":
+    if session['first_time'] and request.endpoint != "explore.embed" and request.endpoint != "general.home":
         flash("Welcome! We have recently redesigned the URL structure for the site to explicity include the language. The site should still function exactly the same as it had. <a href='https://github.com/alexandersimoes/oec/releases/tag/v2.2.0' target='_blank'>Read more about the implications of this update here</a>.", "first_time")
 
     lang = request.args.get('lang', None)
@@ -322,7 +322,8 @@ def home(lang=None):
     current_app = App.query.filter_by(type="tree_map").first_or_404()
     default_build = Build.query.filter_by(app=current_app, name_id=1).first_or_404()
     default_build.set_options(origin=c, dest="all", product="show", classification="hs")
-
+    
+    flash("Welcome! We have recently redesigned the URL structure for the site to explicity include the language. The site should still function exactly the same as it had. <a href='https://github.com/alexandersimoes/oec/releases/tag/v2.2.0' target='_blank'>Read more about the implications of this update here</a>.", "first_time")
     return render_template("home.html", default_build=default_build)
 
 @mod.route('iframe_test/')
