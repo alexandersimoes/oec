b8d47e63d7b5c76371a250fa591685039736c352
Alex Simoes <alexandersimoes@gmail.com>
2015-06-01 13:07:26 -0400

    add common files used by all scripts

---
+++ scripts/common/db_importer.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+    Import a Directory to DB
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Example Usage
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    python db_importer.py --dir=data/baci/2012/ --name=hedu
+
+"""
+
+import click
+import os, sys, fnmatch
+import re
+
+pattern = re.compile('(\w+).tsv(.bz2)*')
+
+
+def parse_table(t, dbname):
+    m = pattern.search(t)
+    if m:
+        return dbname + "_" + m.group(1)
+
+# via http://stackoverflow.com/questions/13299731/python-need-to-loop-through-directories-looking-for-txt-files
+def findFiles (path, filter):
+    for root, dirs, files in os.walk(path):
+        for file in fnmatch.filter(files, filter):
+            yield os.path.join(root, file)
+
+@click.command()
+@click.option('-d', '--dir', default='.', type=click.Path(exists=True), prompt=False, help='Directory for tsv files.')
+@click.option('-a', '--attr_type', help='Attribute Type', type=click.Choice(['hs', 'sitc']), default='hs')
+@click.option('-r', '--revision', help='Product Classification Revision', type=click.Choice(['92', '96', '02', '07']), default='92')
+def main(dir, attr_type, revision):
+    attr_revision = "{}{}".format(attr_type, revision)
+    
+    for f in findFiles(dir, '{}*.tsv*'.format(attr_revision)):
+        bzipped = False
+        
+        print f, "Processing"
+        if f.endswith("bz2"):
+            bzipped = True
+            os.system("bunzip2 -k " + f)
+            f = f[:-4]
+        
+        handle = open(f)
+        
+        m = pattern.search(f)
+        if m:
+            tbl = m.group(1)
+            if "attr" in tbl:
+                tbl = tbl.replace('hs92_', '')
+                if bzipped: os.remove(f)
+                continue
+            # return dbname + "_" + m.group(1)
+        
+        
+        # tablename = parse_table(f, name)
+        print "table name =", tbl
+        header = handle.readline().strip()
+        fields = header.split('\t')
+            
+        fields = [x for x in fields]
+
+        fields = ",".join(fields)
+
+        cmd = '''mysql -uroot $OEC_DB_NAME -e "LOAD DATA LOCAL INFILE '{}' INTO TABLE {} FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES ({});" '''.format(f, tbl, fields)
+        # print cmd
+        os.system(cmd)
+
+        # delete bunzipped file
+        if bzipped:
+            os.remove(f)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/common/growth_calc.py
@@ -0,0 +1,92 @@
+# compute_growth.py
+# -*- coding: utf-8 -*-
+"""
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    python scripts/common/growth_calc.py \
+        data/baci/2012/hs92_yp.tsv.bz2 \
+        data/baci/2011/hs92_yp.tsv.bz2 \
+        --years=1 \
+        --cols=export_val,import_val \
+        --strcasts=hs92_id \
+        -o data/baci/2012
+
+"""
+
+''' Import statements '''
+import os, sys, time, bz2, click
+import pandas as pd
+import numpy as np
+import re
+
+
+def parse_table_name(t):
+    pattern = re.compile('(\w+).tsv(.bz2)*')
+    m = pattern.search(t)
+    if m:
+        return m.group(1)
+         
+def do_growth(t_name, tbl, tbl_prev, cols, years_ago=1, hs_revision=92):
+    '''Growth rate'''
+    pk_lookup = {"o": "origin_id", "d": "dest_id", "p": "hs{}_id".format(hs_revision)}
+    t_namelook = t_name.split("_")[1]
+    pk = [pk_lookup[letter] for letter in t_namelook if letter != 'y']
+
+    tbl = pd.merge(tbl, tbl_prev[pk + cols], how='left', left_on=pk, right_on=pk )
+    
+    for orig_col_name in cols:
+        print "DOING", orig_col_name
+        
+        # percent growth
+        new_col_name = orig_col_name + "_growth_pct"
+        if years_ago > 1:
+            new_col_name = "{0}_{1}".format(new_col_name, years_ago)
+        tbl[new_col_name] = ((1.0 * tbl[orig_col_name+"_x"]) / (1.0*tbl[orig_col_name + "_y"])) ** (1.0/years_ago) - 1
+        
+        # percent growth
+        new_col_name = orig_col_name + "_growth_val"
+        if years_ago > 1:
+            new_col_name = "{0}_{1}".format(new_col_name, years_ago)
+        tbl[new_col_name] = tbl[orig_col_name+"_x"] - tbl[orig_col_name + "_y"]
+    for colname in cols:
+        del tbl[colname + "_y"]
+        tbl.rename(columns={ c+"_x" : c for c in cols }, inplace=True)
+        # tbl[new_col_name] = tbl3[new_col_name] # -- this is safe because of the left merge
+    return tbl
+
+@click.command()
+@click.argument('original_file', type=click.Path(exists=True))
+@click.argument('growth_file', type=click.Path(exists=True))
+@click.option('-c', '--cols', prompt='Columns separated by commas to compute growth', type=str, required=True)
+@click.option('-y', '--years', prompt='years between data points', type=int, required=False, default=1)
+@click.option('-s', '--strcasts', type=str, required=False)
+@click.option('-r', '--hs_revision', help='HS Revision', type=click.Choice(['92', '96', '02', '07']), default='92')
+@click.option('output_path', '--output', '-o', help='Path to save files to.', type=click.Path(), required=True, prompt="Output path")
+def main(original_file, growth_file, cols, output_path, hs_revision, years, strcasts):
+    start = time.time()
+    
+    converters = {}
+    if strcasts:
+        strcastlist = strcasts.split(",")
+        converters = {x:str for x in strcastlist}
+    
+    og_df = pd.read_csv(original_file, sep="\t", converters=converters, compression="bz2", na_values=['\N', None])
+    growth_df = pd.read_csv(growth_file, sep="\t", converters=converters, compression="bz2", na_values=['\N', None])
+
+    col_names = cols.split(",")
+    print "CALCULATING growth for the following columns:", col_names
+    
+    t_name = parse_table_name(original_file)
+
+    new_df = do_growth(t_name, og_df, growth_df, col_names, years, hs_revision)
+    
+    
+    print "GOT TABLE NAME OF", t_name
+    if not t_name:
+        t_name = "noname"
+    new_file_path = os.path.abspath(os.path.join(output_path, "{0}.tsv.bz2".format(t_name)))
+    new_df.to_csv(bz2.BZ2File(new_file_path, 'wb'), sep="\t", index=False, float_format="%.3f", na_rep="\N")
+    
+    print("--- %s minutes ---" % str((time.time() - start)/60))
+
+if __name__ == "__main__":
+    main()
