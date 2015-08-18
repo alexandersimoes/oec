79db1f5e5d7ac1621b3e12aa527f45d7bbaa67b4
Dave Landry <landry.dave@gmail.com>
2015-08-18 17:24:12 -0400

    hooked up rankings into nav and new flags

--- oec/rankings/views.py
+++ oec/rankings/views.py
@@ -29,6 +29,10 @@ def rankings_redirect_nolang(category=None, year=None):
 
 mod = Blueprint('rankings', __name__, url_prefix='/<any("ar","de","el","en","es","fr","he","hi","it","ja","ko","mn","nl","ru","pt","tr","zh"):lang>/rankings')
 
+@mod.before_request
+def before_request():
+    g.page_type = mod.name
+
 @mod.url_value_preprocessor
 def get_profile_owner(endpoint, values):
     lang = values.pop('lang')
@@ -43,6 +47,7 @@ def rankings_redirect():
 # don't cache because downloading will not be possible
 # @view_cache.cached(timeout=2592000, key_prefix=make_cache_key)
 def rankings(category=None, year=None):
+    g.page_sub_type = category
     try:
         depth = int(request.args.get('depth', 4))
     except:
@@ -52,10 +57,10 @@ def rankings(category=None, year=None):
     if download:
         s = StringIO()
         writer = csv.writer(s)
-    
+
     if category == None:
         category = "country"
-    
+
     if year == None:
         download_all = True if download else False
         year = available_years[category][-1]
@@ -63,7 +68,7 @@ def rankings(category=None, year=None):
         return redirect(url_for('.rankings', lang=g.locale, category=category, year=available_years[category][0]))
     elif year < available_years[category][0]:
         return redirect(url_for('.rankings', lang=g.locale, category=category, year=available_years[category][-1]))
-    
+
     # if category == "sitc":
     #     Yp = getattr(db_data, "sitc_models").Yp
     #     Attr, Attr_name, Attr_data, attr_id, index, rank, delta = [Sitc, Sitc_name, Yp, "sitc_id", "pci", "pci_rank", "pci_rank_delta"]
@@ -73,7 +78,7 @@ def rankings(category=None, year=None):
     #     Yp = getattr(db_data, "hs92_models").Yp
     #     Attr, Attr_name, Attr_data, attr_id, index, rank, delta = [Hs, Hs_name, Yp, "hs_id", "pci", "pci_rank", "pci_rank_delta"]
     #     cols = [gettext("Rank"), "", "HS", gettext("Product"), "PCI Value"]
-    
+
     if category == "country":
         Attr, Attr_name, Attr_data, attr_id, index, rank, delta = [Country, Country_name, Yo, "origin_id", "eci", "eci_rank", "eci_rank_delta"]
         cols = [gettext("Rank"), "", "Abbrv", gettext("Country"), "ECI Value"]
@@ -84,29 +89,29 @@ def rankings(category=None, year=None):
         attr_id = "{}_id".format(category)
         index, rank, delta = ["pci", "pci_rank", "pci_rank_delta"]
         cols = [gettext("Rank"), "", category.upper(), gettext("Product"), "PCI Value"]
-        
-    
+
+
     rankings = db.session.query(Attr, Attr_name, Attr_data) \
                 .filter(getattr(Attr_name, attr_id) == Attr.id) \
                 .filter(getattr(Attr_data, attr_id) == Attr.id) \
                 .filter(Attr_name.lang == g.locale) \
                 .filter(getattr(Attr_data, index) != None)
-    
+
     if category != "country":
         x = depth+2
         x = "{}".format(x)
         rankings = rankings.filter(getattr(Attr_data, "{}_id_len".format(category)) == x)
-    
+
     if download_all:
         title = "{0}_all_ranking_{1}".format(category, g.locale)
         rankings = rankings.order_by(Attr_data.year).order_by(getattr(Attr_data, rank))
     else:
         title = "{0}_{1}_ranking_{2}".format(category, year, g.locale)
         rankings = rankings.filter(Attr_data.year == year).order_by(getattr(Attr_data, rank))
-    
+
     # raise Exception(utils.compile_query(rankings))
     rankings = rankings.all()
-    
+
     if download:
         cols = [gettext("Year")] + cols
         writer.writerow([unicode(c).encode("utf-8") for c in cols])
@@ -118,10 +123,10 @@ def rankings(category=None, year=None):
                                 getattr(r[2], index)])
         content_disposition = "attachment;filename={0}.csv".format(title)
         content_disposition = content_disposition.replace(",", "_")
-        return Response(s.getvalue(), 
-                            mimetype="text/csv;charset=UTF-8", 
+        return Response(s.getvalue(),
+                            mimetype="text/csv;charset=UTF-8",
                             headers={"Content-Disposition": content_disposition})
-    
+
     else:
         return render_template("rankings/index.html",
                                 category=category,
