57d612aaf0d6ffc4d167130c28aa8a632697f23f
Dave Landry <landry.dave@gmail.com>
2015-09-18 14:52:15 -0400

    changed default tree map depth on mobile

--- oec/assets/js/configs/tree_map.js
+++ oec/assets/js/configs/tree_map.js
@@ -3,27 +3,27 @@ function show_all_years(){
   var ui = viz.ui().filter(function(u){
     return u.value[0] != "Show all years"
   })
-  
+
   // hide viz and show "loading"
   d3.select("#viz").style("display", "none");
   d3.select("#loading").style("display", "block");
-  
+
   // reformat data url aka replace current year with "all"
   var data_url = build.data_url;
   data_url = data_url.split("/");
   data_url[3] = "all";
   data_url = data_url.join("/");
-  
+
   var q = queue()
     .defer(d3.json, data_url)
     .defer(d3.json, build.attr_url)
     .await(function(error, raw_data, raw_attrs){
-  
+
       var attrs = format_attrs(raw_attrs, build)
       var data = format_data(raw_data, attrs, build)
-     
+
       viz.data(raw_data.data).attrs(attrs).ui(ui).draw();
-      
+
       d3.select("#viz").style("display", "block");
       d3.select("#loading").style("display", "none");
 
@@ -41,9 +41,9 @@ configs.tree_map = function(build, container) {
   else {
     var depth_ui = {"method":"depth", "value":[{"HS2": 0}, {"HS4":1}, {"HS6":2}], "label":"Depth"}
   }
-  
+
   return {
-    "depth": 1,
+    "depth": window.innerWidth < 768 ? 0 : 1,
     "shape": "square",
     "labels": {"align": "start", "valign":"top"},
     "color": "color",
