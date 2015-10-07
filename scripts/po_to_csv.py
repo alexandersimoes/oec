b8755d9ab4871ac504c738e13f8fcc138a4f06d4
Alex Simoes <alexandersimoes@gmail.com>
2015-10-06 20:03:18 -0400

    script to output po file to csv

---
+++ scripts/po_to_csv.py
@@ -0,0 +1,29 @@
+import sys, click, re, csv
+
+def clean_str(s):
+    return s.strip().replace('\n', '').replace('"', '')
+
+@click.command()
+@click.argument('pofile', type=click.File())
+@click.option('-l', '--lang', prompt='Language', required=True)
+def read_po(pofile, lang):
+    all_trans = []
+    contents = pofile.read()
+    found = re.split('\n\s*\n', contents)
+    eng_regex = re.compile('msgid "(.*)msgstr', re.DOTALL)
+    foreign_regex = re.compile('msgstr(.*)', re.DOTALL)
+    for trans in found:
+        if "#, python-format" not in trans:
+            eng_found = eng_regex.findall(trans, re.DOTALL)
+            if eng_found:
+                english_txt = clean_str(eng_found[0])
+                for_found = foreign_regex.findall(trans, re.DOTALL)
+                foreign_txt = clean_str(for_found[0])
+                all_trans.append([english_txt, foreign_txt])
+    
+    with open('{}_trans.tsv'.format(lang), 'w') as fp:
+        a = csv.writer(fp, delimiter='\t')
+        a.writerows(all_trans)
+
+if __name__ == '__main__':
+    read_po()
