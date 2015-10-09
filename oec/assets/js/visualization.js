297fbb33dca311b18df1dedf4064e94d7969dc60
Alex Simoes <alexandersimoes@gmail.com>
2015-10-09 18:16:13 -0400

    made tooltip small on small windows for rings and geo map

--- oec/assets/js/visualization.js
+++ oec/assets/js/visualization.js
@@ -114,7 +114,7 @@ var visualization = function(build, container) {
         .error(false)
         .ui(ui)
         .tooltip({
-          "large": viz_width < 768 ? viz_width * 0.9 : 200,
+          "large": (viz_width < 768 && build.viz.slug != "rings" && build.viz.slug != "geo_map") ? viz_width * 0.9 : 200,
           "stacked": viz_width < 768 ? true : false
         })
         .draw();
