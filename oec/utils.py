c4d0c78a5e6714069cce2febca36461cf29741e9
Dave Landry <landry.dave@gmail.com>
2015-10-16 10:13:03 -0400

    added median util

--- oec/utils.py
+++ oec/utils.py
@@ -90,6 +90,15 @@ def format_currency(value):
 def format_percent(value):
     return "{:.2g}%".format(value)
 
+def median(lst):
+    lst = sorted(lst)
+    if len(lst) < 1:
+        return None
+    if len(lst) %2 == 1:
+        return lst[((len(lst)+1)/2)-1]
+    else:
+        return float(sum(lst[(len(lst)/2)-1:(len(lst)/2)+1]))/2.0
+
 def langify(path, lang):
     possible_langs = [l[0] for l in g.supported_langs]
     url_parts = path.split("/")
