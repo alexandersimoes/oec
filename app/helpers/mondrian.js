6ddca76103aa5eeb39e4c46248575e2e974f1c18
Marcio Porto <mflporto@gmail.com>
2018-08-06 17:44:59 -0400

    saving progress

--- app/helpers/mondrian.js
+++ app/helpers/mondrian.js
@@ -1,6 +1,6 @@
 import {Client as MondrianClient} from "mondrian-rest-client";
 
-const CUBE_API = "http://oeccube.datawheel.us:9292/";
+const CUBE_API = "http://oec-sandbox.datawheel.us/cubes/";
 const client = new MondrianClient(CUBE_API);
 
 export default client;
