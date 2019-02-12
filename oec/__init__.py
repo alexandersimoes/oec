9b472eee105a4cf5bafea667711997bdfacade4e
Alexander Simoes <alexandersimoes@gmail.com>
2019-02-12 13:11:20 -0500

    bumps sitc data up to 2017

--- oec/__init__.py
+++ oec/__init__.py
@@ -50,7 +50,7 @@ db = SQLAlchemy(app)
 view_cache = Cache(app)
 
 # Global Latest Year Variables
-available_years = {"sitc": range(1962, 2017), "hs92": range(1995, 2018), \
+available_years = {"sitc": range(1962, 2018), "hs92": range(1995, 2018), \
                     "hs96": range(1998, 2018), "hs02": range(2003, 2018), \
                     "hs07": range(2008, 2018), "country": range(1962, 2018)}
 
