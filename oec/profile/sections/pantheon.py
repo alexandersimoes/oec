2471f8102f4b592cd9a6cd7509467f4902ec1a1e
Alexander Simoes <alexandersimoes@gmail.com>
2019-12-02 19:11:10 -0500

    removes comment

--- oec/profile/sections/pantheon.py
+++ oec/profile/sections/pantheon.py
@@ -20,7 +20,6 @@ def make_pantheon_section(pantheon_id, attr):
         memorable_people_as_links = [format_pantheon_link(p) for p in memorable_people]
         memorable_people_as_links_str = u", ".join(memorable_people_as_links[:-2] + [u" and ".join(memorable_people_as_links[-2:])])
         pantheon_fields_subtitle.append(u"The most memorable people born within the present day boundries of {} include {}.".format(country_name, memorable_people_as_links_str))
-        # raise Exception(u"The most memorable people born within the present day boundries of {} include {}.".format(country_name, memorable_people_as_links_str))
     pantheon_fields_subtitle.append(u"<a target='_blank' class='viz_link' href='https://pantheon.world/explore/viz?viz=treemap&show=occupations&years=-3501,2015&place={}'>{} <i class='fa fa-external-link'></i></a>".format(pantheon_id, _("Explore on Pantheon")))
     
     pantheon_cities_iframe = "https://pantheon.world/explore/viz/embed?viz=treemap&show=places&years=-3501,2015&place={}".format(pantheon_id)
